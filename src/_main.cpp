9429cf772ca869a052600df60ef088f0f1881c99
user666 <user95401@gmail.com>
2024-10-30 22:24:54 +0400

    хуета

--- src/_main.cpp
+++ src/_main.cpp
@@ -1,8 +1,14 @@
 #include "_main.hpp"
 
-#include <Geode/modify/MenuGameLayer.hpp>
-class $modify(MenuGameLayerColors, MenuGameLayer) {
-    $override cocos2d::ccColor3B getBGColor(int p0) {
-        return GameToolbox::transformColor(MenuGameLayer::getBGColor(p0), cchsv(1, 0.3, 0.6, 0, 0));
+#include <Geode/modify/FMODAudioEngine.hpp>
+class $modify(FMODAudioEngineExt, FMODAudioEngine) {
+    $override void playMusic(gd::string path, bool shouldLoop, float fadeInTime, int channel) {
+        if (string::contains(path, "menuLoop")) {
+            FMODAudioEngine::playMusic("MenuLoop_MelodyBass.mp3"_spr, shouldLoop, fadeInTime, channel);
+            FMODAudioEngine::playEffectAdvanced("MenuLoop_Vocal.mp3"_spr, 1, 1, SETTING(double, "MenuLoop_Vocal"), 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0);
+            FMODAudioEngine::playEffectAdvanced("MenuLoop_Drum.mp3"_spr, 1, 1, SETTING(double, "MenuLoop_Drum"), 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0);
+            return;
+        }
+        return FMODAudioEngine::playMusic(path, shouldLoop, fadeInTime, channel);
     }
 };
